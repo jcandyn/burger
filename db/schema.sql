c75ad7dfc70414749cc92cd8ad339023a8ba459d
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-01 17:29:12 -0400

    inital files upload

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+DROP TABLE IF EXISTS burgers;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN
+);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("La Bestia", 0);
+SELECT * FROM burgers;
