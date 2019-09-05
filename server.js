bb0fa0df00f035e20f7176f29c42284deacf0c39
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-04 22:24:01 -0400

    fixed server

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-var express = require("express")
+/* Set up the server file to require npm packages, express middleware, body-parser, methodOverride, espress-handlebars and the handlebars engine
+*/
+var express = require('express');
+var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var app = express();
+var router = require('./controllers/burgers_controller.js');
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
+// app.use(methodOverride('_method'));
+
+//setup handlebars templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use('/', router);
+
+// Set up the server
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("App is listening on port " + port);
+});
