80e410062f392683cd31851b3a5eee88e5babf71
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-07 16:37:39 -0400

    updated handlebars

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var exphbs = require('express-handlebars');
 var app = express();
 var router = require('./controllers/burgers_controller.js');
 
-// Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(process.cwd() + '/public'));
-
 app.use(bodyParser.urlencoded({
   extended: false
 }));
