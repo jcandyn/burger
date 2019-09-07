051eb9b91ef620a20c48575299a6e86c0b74e545
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-06 21:36:37 -0400

    trying to fix heroku

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ var app = express();
 var router = require('./controllers/burgers_controller.js');
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
@@ -22,6 +25,7 @@ app.set('view engine', 'handlebars');
 
 
 app.use('/', router);
+app.use(express.static("public"));
 
 // Set up the server
 var port = process.env.PORT || 8080;
